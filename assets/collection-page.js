d3c5ec0198bd43c0018c23f8a06e09b2203c777e
kyliancrt <kylian.carette@outlook.fr>
2022-10-28 09:23:53 +0200

    Update collection-page.js

--- assets/collection-page.js
+++ assets/collection-page.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //listFiltres
     if ($(window).width() < 1025) {
-        $(".blocFilters li.active").click(function(event) {
-    event.preventDefault();
+        $(".blocFilters li.active").click(function (event) {
+            event.preventDefault();
 
             if ($(".blocFilters ul").hasClass("active")) {
                 $(".blocFilters ul").removeClass("active");
@@ -45,41 +45,41 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if ($(window).width() < 1025) {
-    //popupPreviewShow
-  
-    $(".template-collection .productsList li > div > :nth-child(2)").click(function () {
-        $(".popupPreview").addClass("active");
-        $('body').css('overflow', 'hidden');
-    });
+        //popupPreviewShow
 
-    //popupPreviewHide
-    $(".popupPreviewHide").click(function () {
-        $(".popupPreview").removeClass("active");
-        $('body').css('overflow', 'visible');
-    });
-  
-    //listRoomsDisplay
-    $(".listRooms >div").click(function () {
-        if ($(".listRooms").hasClass("active")) {
-            $(".listRooms").removeClass("active");
-        }
-        else {
-            $(".listRooms").addClass("active");
-        }
-    });
-      
+        $(".template-collection .productsList li > div > :nth-child(2)").click(function () {
+            $(".popupPreview").addClass("active");
+            $('body').css('overflow', 'hidden');
+        });
+
+        //popupPreviewHide
+        $(".popupPreviewHide").click(function () {
+            $(".popupPreview").removeClass("active");
+            $('body').css('overflow', 'visible');
+        });
+
+        //listRoomsDisplay
+        $(".listRooms >div").click(function () {
+            if ($(".listRooms").hasClass("active")) {
+                $(".listRooms").removeClass("active");
+            }
+            else {
+                $(".listRooms").addClass("active");
+            }
+        });
+
     } else {
-      
-    //listRoomsDisplay
-    $(".listRooms").hover(function () {
-        if ($(".listRooms").hasClass("active")) {
-            $(".listRooms").removeClass("active");
-        }
-        else {
-            $(".listRooms").addClass("active");
-        }
-    });
-      
+
+        //listRoomsDisplay
+        $(".listRooms").hover(function () {
+            if ($(".listRooms").hasClass("active")) {
+                $(".listRooms").removeClass("active");
+            }
+            else {
+                $(".listRooms").addClass("active");
+            }
+        });
+
     }
 
 
@@ -92,7 +92,10 @@ document.addEventListener('DOMContentLoaded', function () {
     if ($(".template-collection").hasClass("collectionCollections")) {
         $("body").css("overflow", "hidden");
     }
-  
+
+
+    $('.listFilterBest').addClass('active');
+
 
 
 });
